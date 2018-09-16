952e1c61e1e06b8263cf0a685dbe5bbe70e6d826
Catherine Salazar <catcreates13@gmail.com>
2018-09-16 19:52:23 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,9 +30,9 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # title == "Home Alone"
   # release_date == 1990
 
-  # Movie.create do |m|
-  #   binding.pry
-  # end
+  Movie.create do |m|
+    binding.pry
+  end
 end
 
 def can_get_the_first_item_in_the_database
