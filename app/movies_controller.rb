d879f993608560fcb125a3bd9091c179d9431370
Catherine Salazar <catcreates13@gmail.com>
2018-09-16 19:38:51 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -21,7 +21,7 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = attributes
+  movie = can_be_instantiated_and_then_saved
 end
 
 def can_be_created_in_a_block(args = __)
