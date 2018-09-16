2fe26d4262c4cc63f965694f09063bc130f97e01
Catherine Salazar <catcreates13@gmail.com>
2018-09-16 20:05:48 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,7 +30,7 @@ def can_be_created_in_a_block
   # title == "Home Alone"
   # release_date == 1990
 
-  movie = Movie.create do |m|
+  Movie.create do |m|
     m.title = "Home Alone"
     m.release_date = 1990
   end
