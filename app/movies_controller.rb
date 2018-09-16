fc9c0a1c51caea1bddbafeb89ed4309c319273ee
Catherine Salazar <catcreates13@gmail.com>
2018-09-16 20:28:23 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -49,8 +49,10 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find(1).title
+  #Movie.find(1).title
+  sql <<- SQL
   SELECT * FROM movies WHERE id = ? LIMIT 1
+  sql
 end
 
 def can_find_by_multiple_attributes
