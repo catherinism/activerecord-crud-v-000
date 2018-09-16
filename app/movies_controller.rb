ab8a7bbe41b9f58a1ab42faef0750a1c028eb6f7
Catherine Salazar <catcreates13@gmail.com>
2018-09-16 21:04:33 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -83,10 +83,11 @@ end
 
 def can_update_multiple_items_at_once
   # Change title of all movies to "A Movie"
+  binding.pry
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  Movie.all.update_attribute :title, "A Movie"
+
 end
 
 def can_destroy_a_single_item
